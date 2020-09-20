5629be98983eaf191eb408c4e7972790fb148951
Matthias Metzger <noobymatze@yahoo.de>
2020-09-20 19:46:28 +0200

    add some comments

--- src/main/kotlin/io/noobymatze/ramble/parser/Chars.kt
+++ src/main/kotlin/io/noobymatze/ramble/parser/Chars.kt
@@ -7,25 +7,34 @@ import io.noobymatze.ramble.Problem
 interface Chars {
 
     /**
+     * Returns a [Parser], which consumes all characters the match the given [predicate].
      *
-     * @param predicate
-     * @return
+     * Be cautious, this parser never fails. If there are no characters to consumer anymore,
+     * it will just stop.
+     *
+     * @param predicate a predicate
+     * @return a new [Parser]
      */
     fun takeWhile(predicate: (Char) -> Boolean): Parser<Nothing, String> =
         Parser.TakeWhile(predicate)
 
     /**
+     * Returns a [Parser], which consumes at most [n] tokens.
      *
-     * @param n
-     * @return
+     * Be cautious, this parser never fails. If it reaches the end of
+     * input, it will not take the full number of tokens.
+     *
+     * @param n the number of tokens to consume
+     * @return a new [Parser]
      */
     fun take(n: Int): Parser<Nothing, String> =
         Parser.advance(n)
 
     /**
+     * Returns a [Parser], which matches exactly the given [value].
      *
-     * @param value
-     * @return
+     * @param value a value
+     * @return a new [Parser]
      */
     fun string(value: String): Parser<Nothing, String> =
         take(value.length) flatMap { parsed ->
